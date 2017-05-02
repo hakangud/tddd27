4ea485acb8ed131bfae1358335133224896ca457
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-05-02 21:37:24 +0200

    add test stubs to server.py

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
 from flask import request, Flask
 from gevent.pywsgi import WSGIServer
 from geventwebsocket.handler import WebSocketHandler
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello world!"
+    return 'Hello world!'
 
-if __name__ == "__main__":
+@app.route('/signin', methods=['POST'])
+def login():
+    data = "1qaz2wsdx3edc"
+    return json.dumps({"success": True, "message": "You are now signed in", "data": data})
+
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    return json.dumps({"success": True, "message": "You are now signed up"})
+
+@app.route('/signout', methods=['POST'])
+def sign_out():
+    return json.dumps({"success": True, "message": "You are now signed out"})
+
+if __name__ == '__main__':
     http_server = WSGIServer(('', 8000), app, handler_class=WebSocketHandler)
     http_server.serve_forever()
     app.run()
