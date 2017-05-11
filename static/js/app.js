681c254d32c21682ecb2188e24da08f7dfc099c3
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-05-11 10:23:34 +0200

    add register view routing

--- static/js/app.js
+++ static/js/app.js
@@ -3,24 +3,29 @@
 
     angular
         .module('app', ['ngRoute'])
-        .config(config)
+        .config(config);
 
-        config.$inject = ['$routeProvider'];
-        function config($routeProvider) {
-            //console.log("app.js");
-            $routeProvider
-                .when('/', {
-                    controller: 'LoginController',
-                    templateUrl: '/static/login/login.view.html',
-                    controllerAs: 'vm'
-                })
 
-                .when('/home', {
-                    controller: 'HomeController',
-                    templateUrl: '/static/home/home.view.html',
-                    controllerAs: 'vm'
-                })
 
-                .otherwise({ redirectTo: '/' });
-        };
+    config.$inject = ['$routeProvider'];
+    function config($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'LoginController',
+                templateUrl: '/static/login/login.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                templateUrl: '/static/register/register.view.html'
+            })
+
+            .when('/home', {
+                controller: 'HomeController',
+                templateUrl: '/static/home/home.view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/' });
+    };
 })();
