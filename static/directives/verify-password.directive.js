76dae276967191aae7f94694404c4b4f4e3582f5
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-05-15 23:04:08 +0200

    make verify-password IIFE

--- static/directives/verify-password.directive.js
+++ static/directives/verify-password.directive.js
@@ -1,20 +1,22 @@
-'use strict';
+(function () {
+    'use strict';
 
-angular
-    .module('vpwd', [])
-    .directive('verifyPassword', verifyPassword);
+    angular
+        .module('vpwd', [])
+        .directive('verifyPassword', verifyPassword);
 
-function verifyPassword() {
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function (scope, elem, attrs, ctrl) {
-            var firstPassword = '#' + attrs.verifyPassword;
-            $(elem).add(firstPassword).on('keyup', function () {
-                scope.$apply(function () {
-                    ctrl.$setValidity('pwmatch', elem.val() === $(firstPassword).val());
+    function verifyPassword() {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, elem, attrs, ctrl) {
+                var firstPassword = '#' + attrs.verifyPassword;
+                $(elem).add(firstPassword).on('keyup', function () {
+                    scope.$apply(function () {
+                        ctrl.$setValidity('pwmatch', elem.val() === $(firstPassword).val());
+                    });
                 });
-            });
+            }
         }
-    }
-};
+    };
+})();
