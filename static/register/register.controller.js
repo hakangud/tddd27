c3faf820c4f97d18feac1afe3ac9a7ee03df35c5
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-05-15 23:05:32 +0200

    add user.service.js

--- static/register/register.controller.js
+++ static/register/register.controller.js
@@ -8,6 +8,21 @@
     RegisterController.$inject = ['$location'];
     function RegisterController($location) {
         var vm = this;
+
+        vm.register = register;
         console.log("regC");
+
+        function register() {
+            console.log("reg");
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        $location.path('/login');
+                    }
+                    else {
+                        console.log("reg_error");
+                    }
+                })
+        }
     };
 })();
---
+++ static/services/user.service.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('us', [])
+        .factory('UserService', UserService);
+
+    UserService.$inject = ['$http'];
+    console.log("us");
+    function UserService($http) {
+        var service = {};
+
+        service.Create = Create;
+
+        return service;
+
+        function Create(user) {
+            return $http.post('register', user);
+        }
+    }
+})();
