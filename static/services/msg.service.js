a5f4028c513fcdea37a1061ad6b80c3a6396c4b3
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-05-17 16:42:49 +0200

    add service for messages

---
+++ static/services/msg.service.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('msg', [])
+        .factory('MsgService', MsgService);
+
+    MsgService.$inject = ['$rootScope', '$timeout'];
+    function MsgService($rootScope, $timeout) {
+        var service = {};
+
+        service.Success = Success;
+        service.Error = Error;
+
+        return service;
+
+        function Success(message) {
+            $timeout.cancel($rootScope.timer);
+            $rootScope.flash = {
+                message: message,
+                type: 'success'
+            };
+            $rootScope.timer = $timeout(function () {
+                delete $rootScope.flash;
+            }, 5000);
+        }
+
+        function Error(message) {
+            $timeout.cancel($rootScope.timer);
+            $rootScope.flash = {
+                message: message,
+                type: 'error'
+            };
+            $rootScope.timer = $timeout(function () {
+                delete $rootScope.flash;
+            }, 5000);
+        }
+    }
+
+})();
