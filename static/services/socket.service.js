1d01314befa69add399febdbc38fd262a3b31d53
Håkan Gudmundsson <hakgu806@student.liu.se>
2017-05-30 23:57:54 +0200

    cleanup socket service code

--- static/services/socket.service.js
+++ static/services/socket.service.js
@@ -49,45 +49,6 @@
         }
 
         return service;
-
-        /*function listener(data) {
-            var messageObj = data;
-            console.log("Received data from websocket: ", messageObj);
-            // If an object exists with callback_id in our callbacks object, resolve it
-            if(callbacks.hasOwnProperty(messageObj.callback_id)) {
-                console.log(callbacks[messageObj.callback_id]);
-                $rootScope.$apply(callbacks[messageObj.callback_id].cb.resolve(messageObj.data));
-                delete callbacks[messageObj.callbackID];
-            }
-        }*/
-
-        /*function init(email) {
-            var fc = FridgeService.getFridgeContent;
-            $rootScope.ws = new WebSocket('ws://' + location.host + '/websocket');
-            $rootScope.ws.binaryType = 'arraybuffer';
-
-            $rootScope.ws.onopen = function () {
-                console.log('connected');
-                $rootScope.ws.send(email);
-            };
-            $rootScope.ws.onmessage = function (evt) {
-                listener(evt);
-                $rootScope.$apply(function () {
-                    var message = json.parse(evt.data);
-                    console.log(message.action);
-                });
-            };
-            $rootScope.ws.onclose = function () {
-                console.log('connection closed');
-            };
-        }*/
-
-        function listener(data) {
-            var messageObj = data;
-            console.log("Received data from websocket: ", messageObj);
-            // If an object exists with callback_id in our callbacks object, resolve it
-        }
-
     }
 
 })();
